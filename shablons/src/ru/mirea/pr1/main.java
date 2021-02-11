6915a7d2ac6de89057a803853ce75640463217ec
DrUiiD <brusov2001@bk.ru>
2021-02-11 15:43:22 +0300

    Practice 1
    variant 7

--- shablons/src/ru/mirea/pr1/main.java
+++ shablons/src/ru/mirea/pr1/main.java
@@ -1,12 +1,18 @@
 package ru.mirea.pr1;
 import java.util.Comparator;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x,y;
+        x=scanner.nextInt();
+        y=scanner.nextInt();
+
         Comparator<Integer> test = (a, b) ->{
             if (Math.abs(a)>Math.abs(b)) return a;
             else return b;
         };
-        System.out.println(test.compare(12, -34));
+        System.out.println(test.compare(x, y));
     }
 }
